e5aad2db5522322c9e3d7bb41e12e2a470bd1dde
Miguel Lopez <miguel.2211.lopez@gmail.com>
2025-06-11 18:35:21 -0600

    Se agregan dos rutas nuevas y se dan estilos generales a las imagenes

--- app.js
+++ app.js
@@ -7,14 +7,23 @@ app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
+app.use(express.static(path.join(__dirname, "public")))
+
 app.get("/", (req, res) => {
     res.render("homepage/index.ejs")
 })
 
-app.get("/sculptures", (req, res) => {
-    res.render("sculptures/index")
+app.get("/figurines", (req, res) => {
+    res.render("figurines/index")
 })
 
+app.get("/cosplay", (req, res) => {
+    res.render("cosplay/index")
+})
+
+app.get("/props", (req, res) => {
+    res.render("props/index")
+})
 
 app.listen(3000, () => {
     console.log("listening por 3000")
